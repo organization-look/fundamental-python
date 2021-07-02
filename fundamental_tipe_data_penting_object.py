9970c4531e901878b791404654ef4cea515a981e
Aldi <aldiponorogo70@gmail.com>
2021-07-02 16:03:53 +0700

    test

--- fundamental_tipe_data_penting_object.py
+++ fundamental_tipe_data_penting_object.py
@@ -10,4 +10,17 @@ print(kamus_indo_eng)
 print(kamus_indo_eng['anak'])
 print(kamus_indo_eng['istri'])
 
-print
+print('data ini dikirim dari sever gojek,untuk memberikan data infor driver di sekitar  pemakai aplikasi')
+data_dari_server_gojek ={
+    'tanggal' : '2021-7-2',
+    'driverList' : [
+        {'nama':'eko','jarak':10},
+        {'nama':'dwi','jarak':30},
+        {'nama':'joko','jarak':40},
+        {'nama':'tio','jarak':1000}
+    ]
+}
+print(data_dari_server_gojek)
+print(f"\n driver disekitar sini{data_dari_server_gojek['driverList']}")
+print(f"driver disekitar sini nomor#1{data_dari_server_gojek['driverList'][0]}")
+print(f"Driver terdekat {data_dari_server_gojek ['driverList'][0] ['jarak']} meter")
